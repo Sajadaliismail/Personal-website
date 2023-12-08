3636961e7b3435fd38e6f830b5f82ceac18f25a0
Sajadaliismail <sajadaliismail@gmail.com>
2023-12-08 09:12:31 +0530

    new

---
+++ assets/vendor/php-email-form/php-email-form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<form>
+<h2>Contact us</h2>
+<p><label>First Name:</label> <input name="myEmail" type="text" /></p>
+<p><label>Email Address:</label> <input style="cursor: pointer;" name="myEmail" type="text" /></p>
+<p><label>Message:</label>  <textarea name="message"></textarea> </p>
+<p><input type="submit" value="Send" /></p>
+</form>
+</body>
+</html>
